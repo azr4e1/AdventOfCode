5a5561941dfaf78b6ade8b4f480e3d1b1c7def4d
Lorenzo Drumond <drumondlorenzo@posteo.net>
2025-12-11 16:30:00 +0000

    chore: commit save point

--- 2025/Day7/part2.py
+++ 2025/Day7/part2.py
@@ -1,3 +1,6 @@
+from collections import defaultdict
+
+
 class Position:
     def __init__(self, x, y):
         self.x = x
@@ -17,10 +20,10 @@ class Position:
 
 
 def fall_beams(beams):
-    for beam in list(beams):
-        beams.remove(beam)
+    for beam, val in beams.copy().items():
+        del beams[beam]
         beam.y += 1
-        beams.add(beam)
+        beams[beam] = val
 
 
 def split_beam(beam: Position):
@@ -28,29 +31,47 @@ def split_beam(beam: Position):
 
 
 def parse(input: str):
-    beams = set()
+    beams = defaultdict(int)
     lines = input.split("\n")
     initial_line = lines[0]
-    beams.add(Position(initial_line.index("S"), 0))
+    beams[Position(initial_line.index("S"), 0)] = 1
 
     return beams.copy(), lines
 
 
 def get_beams(beams, diagram):
-    split_times = 0
     for y, line in enumerate(diagram):
         for x, el in enumerate(line):
             if el == "^":
                 splitter = Position(x, y)
                 if splitter in beams:
-                    split_times += 1
-                    beams.remove(splitter)
+                    beam_val = beams[splitter]
+                    del beams[splitter]
                     beam1, beam2 = split_beam(splitter)
-                    beams.add(beam1)
-                    beams.add(beam2)
+                    beams[beam1] += beam_val
+                    beams[beam2] += beam_val
         fall_beams(beams)
 
-    return beams, split_times
+    return beams
+
+
+def get_beams_recursive(beam_x, diagram):
+    number_of_beams = 0
+    if not diagram:
+        return 1
+    line = diagram[0]
+    was_split = False
+    for x, el in enumerate(line):
+        if el == "^" and x == beam_x:
+            beam1, beam2 = x-1, x+1
+            no_beams1 = get_beams_recursive(beam1, diagram[1:])
+            no_beams2 = get_beams_recursive(beam2, diagram[1:])
+            number_of_beams += no_beams1 + no_beams2
+            was_split = True
+    if not was_split:
+        number_of_beams += get_beams_recursive(beam_x, diagram[1:])
+
+    return number_of_beams
 
 
 with open("./input.txt") as f:
@@ -58,6 +79,8 @@ with open("./input.txt") as f:
 
 beams, lines = parse(content)
 
-beams, split_times = get_beams(beams, lines)
+beam = list(beams.keys())[0]
+# number_of_beams = get_beams_recursive(beam.x, lines)
+number_of_beams = sum(get_beams(beams, lines).values())
 
-print("Number of beams:", split_times)
+print("Number of beams:", number_of_beams)
